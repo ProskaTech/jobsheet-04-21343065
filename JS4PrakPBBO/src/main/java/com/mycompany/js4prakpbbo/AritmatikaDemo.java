03d53df123f2edbf3733b5b454bedd34f27bd570
21343065 <112673898+21343065@users.noreply.github.com>
2022-09-15 17:25:48 +0700

    Add files via upload
    Nama : PUTI ZAFANIA
    NIM    : 21343065

---
+++ JS4PrakPBBO/src/main/java/com/mycompany/js4prakpbbo/AritmatikaDemo.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.js4prakpbbo;
+
+/**
+ *
+ * created by 21343065_PUTI ZAFANIA
+ */
+public class AritmatikaDemo {
+    public static void main(String[] args){
+        int i = 10;
+        int j = 3;
+        double x = 27.475;
+        double y = 7.22;
+
+        System.out.println("Variable values...");
+        System.out.println("i = " + i);
+        System.out.println("i = " + j);
+        System.out.println("i = " + x);
+        System.out.println("i = " + y);
+
+        //penjumlahan angka
+        System.out.println("Adding...");
+        System.out.println("i + j = " + (i + j));
+        System.out.println("x + y = " + (x + y));
+
+        //pengurangan angka
+        System.out.println("Substracting...");
+        System.out.println("i - j = " + (i - j));
+        System.out.println("x - y = " + (x - y));
+
+        //perkalian angka
+        System.out.println("Multiplying...");
+        System.out.println("i * j = " + (i * j));
+        System.out.println("x * y = " + (x * y));
+
+        //pembagian angka
+        System.out.println("Dividing...");
+        System.out.println("i / j = " + ((float)i / j));
+        System.out.println("x / y = " + (x / y));
+
+        //menghitung hasil modulus dari pembagian
+        System.out.println("Computing the remainder...");
+        System.out.println("i % j = " + (i % j));
+        System.out.println("x % y = " + (x % y));
+
+        //tipe penggabungan
+        System.out.println("Mixing tiper...");
+        System.out.println("j + y = " + (j + y));
+        System.out.println("i * x = " + (i + x));
+    }
+}
